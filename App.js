90429c03fced17a013b90bb2fbacc23dc7e2810a
Eric Cartagena <cartagenaea@gmail.com>
2023-11-22 01:51:37 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import MainNavigation from './navigation/MainNavigation.js';
+import MainScreen from './screens/MainScreen.js';
+import SecondaryScreen from './screens/SecondaryScreen';
+import ThirdScreen from './screens/ThirdScreen';
+
+const App = () => {
+  return (
+    <MainNavigation />
+  )
+}
+
+
+export default App
